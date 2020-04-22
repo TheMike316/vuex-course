cc55579dc22e357ee48507d634079bdea04c1ed8
mike <hornegger@view-elevator.com>
2020-04-22 08:22:47 +0200

    use state in Todos Component

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <h3>Todos</h3>
+        <div class="todos">
+            <div class="todo" v-for="todo in allTodos" :key="todo.id">
+                {{todo.title}}
+            </div>
+        </div>
     </div>
 </template>
 
@@ -14,5 +19,20 @@
 </script>
 
 <style scoped>
+    .todos {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 1rem;
+    }
+
+    .todo {
+        border: 1px solid #ccc;
+        background: #41b883;
+        padding: 1rem;
+        border-radius: 5px;
+        text-align: center;
+        position: relative;
+        cursor: pointer;
+    }
 
 </style>
