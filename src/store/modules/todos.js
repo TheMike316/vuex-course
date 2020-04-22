0169952f2f4530e5bf1428621ff902f2517572e7
mike <hornegger@view-elevator.com>
2020-04-22 08:34:03 +0200

    fetch and set todos from jsonplaceholder

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,29 +1,24 @@
-// import axios from 'axios';
+import axios from 'axios';
 
 const state = {
-    todos: [
-        {
-            id: 1,
-            title: 'Todo One'
-        },
-        {
-            id: 2,
-            title: 'Todo Two'
-        },
-        {
-            id: 3,
-            title: 'Todo Three'
-        },
-    ]
+    todos: []
 };
 
 const getters = {
     allTodos: state => state.todos
 };
 
-const actions = {};
+const actions = {
+    async fetchTodos({commit}) {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=12')
 
-const mutations = {};
+        commit('setTodos', response.data);
+    }
+};
+
+const mutations = {
+    setTodos: (state, todos) => state.todos = todos
+};
 
 export default {
     state,
