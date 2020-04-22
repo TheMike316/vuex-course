237006844dc5d71e0ff744fd82439f55f956a6ad
mike <hornegger@view-elevator.com>
2020-04-22 09:54:13 +0200

    add action to submit event

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h3>Add Todo</h3>
-        <div class="add">
+        <div class="add" @submit="onSubmit">
             <form>
                 <input type="text" v-model="title" placeholder="Add Todo...">
                 <input type="submit" value="Submit">
@@ -11,12 +11,23 @@
 </template>
 
 <script>
+    import {mapActions} from 'vuex';
+
     export default {
         name: "AddTodo",
         data() {
             return {
                 title: ''
             };
+        },
+        methods: {
+            ...mapActions(['addTodo']),
+            onSubmit(e) {
+                e.preventDefault();
+                this.addTodo(this.title);
+
+                this.title = '';
+            }
         }
     }
 </script>
